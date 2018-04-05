557cf4a62426a8b256d6793c1bfd90a5d28567af
dsantosmerino <dsantosmerino@gmail.com>
2018-04-05 13:15:56 +0200

    Modify advertised address to work with dinghy

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :bolt_routing_driver,
-  url: "core1:7687",
+  url: "core1.docker:7687",
   bolt_sips: [
     basic_auth: [username: "neo4j", password: "changeme"],
     pool_size: 10
